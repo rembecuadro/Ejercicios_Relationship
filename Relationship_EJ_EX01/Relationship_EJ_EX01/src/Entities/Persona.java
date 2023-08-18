7f5c0850465254a4fe2f067e96c112630e2f64e0
Remberto J. Cuadro Alvear <134017388+rembecuadro@users.noreply.github.com>
2023-08-17 22:05:08 -0500

    Asignación Personas-Mascotas
    Archivo funcional, ajustar menu y métodos

---
+++ Relationship_EJ_EX01/Relationship_EJ_EX01/src/Entities/Persona.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Entities;
+
+//@author rembe
+
+import java.util.ArrayList;
+
+ 
+public class Persona {
+    
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+    private Integer documento;
+    private ArrayList<Mascota> listaMascotas;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, Integer edad, Integer documento, ArrayList<Mascota> listaMascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.documento = documento;
+        this.listaMascotas = listaMascotas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Integer getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(Integer documento) {
+        this.documento = documento;
+    }
+
+    public ArrayList<Mascota> listaMascotas() {
+        return listaMascotas;
+    }
+
+    public void setMascota(ArrayList<Mascota> listaMascotas) {
+        this.listaMascotas = listaMascotas;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPersona \nNombre= " + nombre +" "+ apellido + "\nEdad= " + edad + "\nID= " + documento + "\nMascota= " + listaMascotas;
+    }
+    
+    
+
+}
---
+++ Relationship_EJ_EX01/Relationship_EJ_EX01/src/relationship_ej_ex01/Relationship_Ej_Ex1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package relationship_ej_ex01;
+
+import Services.Servicio;
+
+/**
+ *
+ * @author rembe
+ */
+public class Relationship_Ej_Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Servicio s = new Servicio();
+        s.menu();
+    }
+}
